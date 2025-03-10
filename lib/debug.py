3c1bd86ec9ac17cdb80d21b9e25bdf830df1199e
kenneth <kennjorothuo@gmail.com>
2025-03-11 02:32:30 +0300

    initial commit

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_sandbox import Student  # Import your Student model
 
-from sqlalchemy_sandbox import Student
-
+# Create an engine that connects to the SQLite database
 engine = create_engine('sqlite:///students.db')
 
+# Create a sessionmaker to interact with the database
+Session = sessionmaker(bind=engine)
+session = Session()
+
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    import ipdb; ipdb.set_trace()  # This will pause the script and allow interactive debugging
+
+    # You can now interact with the database within the ipdb shell
+    # Example query: Retrieve all students from the table
+    students = session.query(Student).all()
+
+    # Print out all the students
+    for student in students:
+        print(f"Student ID: {student.id}, Name: {student.name}")
+
+    # Make sure to close the session when done
+    session.close()
--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
-
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 
+# Create a base class using declarative_base
 Base = declarative_base()
 
+# Define the Student model
 class Student(Base):
-    pass
+    __tablename__ = 'students'  # Define the name of the table
 
+    id = Column(Integer, primary_key=True)  # Define the primary key
+    name = Column(String)  # Define the name column
+
+# Add the script to create the database schema
 if __name__ == '__main__':
-    pass
+    # Set up the engine to point to an SQLite database
+    engine = create_engine('sqlite:///students.db')
+
+    # Create all the tables defined by models inheriting from Base (Student in this case)
+    Base.metadata.create_all(engine)
